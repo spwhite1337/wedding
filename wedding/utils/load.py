ad49d9ec08bfc0e95f1dec879c69a6a2c60620f3
Scott White <spwhite1337@gmail.com>
2020-09-23 23:31:38 -0400

    Plot for time-series

--- wedding/utils/load.py
+++ wedding/utils/load.py
@@ -6,14 +6,17 @@ from config import Config, logger
 
 def load_weather() -> pd.DataFrame:
     logger.info('Loading weather data')
-    df = pd.read_csv(os.path.join(Config.DATA_DIR, 'weather', 'noblesville_weather.csv'))
+    df = pd.read_csv(os.path.join(Config.DATA_DIR, 'wedding', 'noblesville_weather.csv'))
     df['dt_iso'] = pd.to_datetime(df['dt_iso'].str.replace(' \+0000 UTC', ''))
     # Correct for time-zone (it is now in EST)
     df['dt_iso'] = df['dt_iso'] - pd.Timedelta(4, 'H')
+    # Convert temperature to F
+    for t in ['temp', 'temp_min', 'temp_max']:
+        df[t] = (df[t] - 273.15) * (9 / 5) + 32
     # Get important parts of date
     df['year'] = df['dt_iso'].dt.year
     df['month'] = df['dt_iso'].dt.month
-    df['week'] = df['dt_iso'].dt.isocalendar().week
+    df['week'] = df['dt_iso'].dt.week
     df['day'] = df['dt_iso'].dt.day
     df['hour'] = df['dt_iso'].dt.hour
 
--- wedding/utils/weather.py
+++ wedding/utils/weather.py
@@ -10,7 +10,22 @@ class WeatherAnalyzer(object):
     source_data_fp = os.path.join(Config.DATA_DIR, 'wedding', 'noblesville_weather.csv')
     plots_dir = Config.PLOTS_DIR
 
-    # Time-series by hour on day or range-of-days plotting mean / std or median / std
+    # Time-series by hour on month-day or range-of-days plotting mean / std or median / std
+    def time_series_by_day(self, month: int, day: int):
+        df = load_weather()
+        df = df[(df['month'] == month) & (df['day'] == day)]
+        df = df.groupby('hour').agg(temp=('temp', 'mean'), temp_std=('temp', 'std')).reset_index()
+
+        plt.figure(figsize=(8, 8))
+        plt.plot(df['hour'], df['temp'])
+        plt.fill_between(df['hour'], df['temp'] - df['temp_std'], df['temp'] + df['temp_std'], alpha=0.5)
+        plt.xlabel('Hour of Day')
+        plt.ylabel('Temperature')
+        plt.title('Date: {}-{}'.format(month, day))
+        plt.grid(True)
+        plt.tight_layout()
+        plt.savefig(os.path.join(self.plots_dir, 'time_series_by_day_{}_{}.png'.format(month, day)))
+        plt.close()
 
     # Distribution of temperatures by month-day-hour
 
